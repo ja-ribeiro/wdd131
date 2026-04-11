b9870d085891f1f916707c3bf76fddc23c3b4092
ja-ribeiro <jribeiro1@byupatwhay.edu>
2026-04-11 18:48:47 -0300

    tive que arrumar tudo um pouco

---
+++ scripts/form-review.js
@@ -0,0 +1,12 @@
+let count = localStorage.getItem("reviewCount");
+
+if (count === null) {
+  count = 0;
+}
+
+count++;
+
+localStorage.setItem("reviewCount", count);
+
+document.getElementById("counter").textContent =
+  "Total Reviews Submitted: " + count;
