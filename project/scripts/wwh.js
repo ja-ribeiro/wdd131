67848cd27800323600661cd0c47e92b9d8ba27bd
ja-ribeiro <jribeiro1@byupatwhay.edu>
2026-04-13 21:35:39 -0300

    eu mudei bastante coisa de lugar, principalemte imagens. ainda estou vendo o que fica melhor

--- project/scripts/wwh.js
+++ project/scripts/wwh.js
@@ -5,18 +5,6 @@ menuBtn.addEventListener("click", () => {
   navMenu.classList.toggle("open");
 });
 
-// ARRAY + OBJECT
-const characters = [
-  { name: "Harry Potter", house: "Gryffindor" },
-  { name: "Hermione Granger", house: "Gryffindor" },
-  { name: "Draco Malfoy", house: "Slytherin" }
-];
-
-// ARRAY METHOD (AGORA TEM)
-const characterNames = characters.map(c => c.name);
-console.log(`Characters: ${characterNames.join(", ")}`);
-
-// SAVE FAVORITE
 function saveFavorite() {
   const select = document.getElementById("characterSelect");
   const result = document.getElementById("result");
@@ -31,19 +19,4 @@ function saveFavorite() {
   localStorage.setItem("favoriteCharacter", value);
 
   result.textContent = `Your favorite character is ${value}.`;
-}
-
-// FORM
-const form = document.getElementById("contactForm");
-
-if (form) {
-  form.addEventListener("submit", function(e) {
-    e.preventDefault();
-
-    const name = document.getElementById("name").value;
-
-    const message = `Thank you, ${name}. Your message has been sent successfully!`;
-
-    document.getElementById("formResult").textContent = message;
-  });
 }
